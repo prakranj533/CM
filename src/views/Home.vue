073d80f3ad2afc44f22f4ea19de3842a74491cec
shailesh <nighojkar111@gmail.com>
2021-04-14 02:28:16 +0530

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="home">
     <v-list-item>
+      <v-list-item-title class="text-h5">Probabilistic Career Maps on top</v-list-item-title>
       <v-spacer></v-spacer>
       <v-btn text @click="goToCareerPathFinder">Career Path Finder</v-btn>
     </v-list-item>
     <Graph ref="graph" :nodeMap="nodeMap" />
+    <v-list-item-title class="my-8 text-center text-body-2">Feedback: lifelonglearning.in@gmail.com</v-list-item-title>
   </div>
 </template>
 

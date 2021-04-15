f8f84a0b16a04a6cbbf718319a4424b61d684bf1
shailesh <nighojkar111@gmail.com>
2021-04-15 16:26:16 +0530

    highlight nodes

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -48,6 +48,7 @@ export default {
       const svg = d3.select("#graph-svg");
       svg.attr("width", width);
       svg.attr("height", height);
+      svg.on("click", () => d3.event.target.tagName !== "circle" && (this.idsToHighlight = []));
       const color = d3.scaleOrdinal(d3.schemeCategory10);
 
       /* prettier-ignore */
@@ -75,20 +76,19 @@ export default {
         .append("circle")
         .attr("fill", (d) => color(d.id))
         .attr("r", 6);
-      // .attr("onmouseover", "evt.target.setAttribute('r', 9);")
-      // .attr("onmouseout", "evt.target.setAttribute('r', 6);")
-      //   this.handleNodeHover(circles);
 
       nodes.append("text").text((d) => d.name);
+
       nodes.on("click", (d) => this.handleNodeClick(d.id, nodes));
+      this.handleHover(nodes);
 
       const forceSimulation = this.getForceSimulation(width, height, data, links, nodes);
       this.handleNodeDrag(nodes, forceSimulation);
     },
-    handleNodeClick(nodeId, nodes) {
-      this.idsToHighlight = [nodeId, ...this.nodeMap[nodeId].paths.map((e) => e.to)];
-      nodes.select("circle").attr("class", (d) => (this.idsToHighlight.includes(d.id) ? "highlight" : "blur"));
-      nodes.select("text").attr("class", (d) => (this.idsToHighlight.includes(d.id) ? "highlight" : "blur"));
+    handleNodeClick() {
+      // this.idsToHighlight = [nodeId, ...this.nodeMap[nodeId].paths.map((e) => e.to)];
+      // nodes.select("circle").attr("class", (d) => (this.idsToHighlight.includes(d.id) ? "highlight" : "blur"));
+      // nodes.select("text").attr("class", (d) => (this.idsToHighlight.includes(d.id) ? "highlight" : "blur"));
       //   circles
       //     .on("mouseover", (d) => {
       //       this.idsToHighlight = [d.id, ...this.nodeMap[d.id].paths.map((e) => e.to)];
@@ -96,7 +96,7 @@ export default {
       //     .on("mouseout", () => {
       //       this.idsToHighlight = [];
       //     });
-      this.$emit("nodeClicked", nodeId);
+      // this.$emit("nodeClicked", nodeId);
     },
     handleNodeDrag(nodes, forceSimulation) {
       const drag = d3
@@ -117,6 +117,26 @@ export default {
           d.fy = null;
         });
       nodes.call(drag);
+    },
+    handleHover(nodes) {
+      nodes
+        .select("circle")
+        .attr("onmouseover", "evt.target.setAttribute('r', 8)")
+        .attr("onmouseout", "evt.target.setAttribute('r', 6)")
+        .on("mouseover", (d) => {
+          this.idsToHighlight = [d.id, ...this.nodeMap[d.id].paths.map((e) => e.to)];
+          nodes
+            .classed("blur", true)
+            .filter((d) => this.idsToHighlight.includes(d.id))
+            .classed("highlight", true)
+            .classed("blur", false);
+        })
+        .on("mouseout", () => {
+          this.idsToHighlight = [];
+          nodes /* prettier-ignore */
+            .classed("highlight", false)
+            .classed("blur", false);
+        });
     },
     getForceSimulation(width, height, data, links, nodes) {
       const density = 0.08680792891319207;
@@ -159,22 +179,23 @@ export default {
           const yMax = (height - NODE_RADIUS - scale.dy) / scale.factor;
           return (d.y = Math.max(yMin, Math.min(yMax, d.y)));
         };
-        const circle = nodes
+
+        nodes
           .select("circle")
           .attr("cx", (d) => getX(d))
           .attr("cy", (d) => getY(d));
 
-        const circleText = nodes
+        nodes
           .select("text")
           .attr("x", (d) => getX(d) - (d.name.length * 14) / 4)
           .attr("y", (d) => getY(d) - 8);
 
-        const highlight = (d) => this.idsToHighlight.includes(d.id);
-        if (this.idsToHighlight.length) {
-          circle.attr("r", (d) => (highlight(d) ? 9 : 6));
-          circle.attr("class", (d) => (highlight(d) ? "highlight" : "blur"));
-          circleText.attr("class", (d) => (highlight(d) ? "highlight" : "blur"));
-        }
+        // const highlight = (d) => this.idsToHighlight.includes(d.id);
+        // if (this.idsToHighlight.length) {
+        //   // circle.attr("r", (d) => (highlight(d) ? 9 : 6));
+        //   // circle.attr("class", (d) => (highlight(d) ? "highlight" : "blur"));
+        //   // circleText.attr("class", (d) => (highlight(d) ? "highlight" : "blur"));
+        // }
       };
     },
   },
@@ -191,14 +212,14 @@ export default {
       stroke: #000;
       stroke-opacity: 0.25;
       transition: opacity 150ms linear, stroke-opacity 150ms linear;
-      &.highlight {
-        opacity: 1;
-        stroke-opacity: 1;
-      }
-      &.blur {
-        opacity: 0.2;
-        stroke-opacity: 1;
-      }
+      // &.highlight {
+      //   opacity: 1;
+      //   stroke-opacity: 1;
+      // }
+      // &.blur {
+      //   opacity: 0.2;
+      //   stroke-opacity: 1;
+      // }
     }
     text {
       font-size: 9px;
@@ -210,44 +231,49 @@ export default {
       -ms-user-select: none;
       user-select: none;
       /*********************/
-      &.highlight {
-        opacity: 1;
-      }
-      &.blur {
-        opacity: 0.2;
-      }
+      // &.highlight {
+      //   opacity: 1;
+      // }
+      // &.blur {
+      //   opacity: 0.2;
+      // }
     }
   }
   .nodes {
-    circle {
-      &:hover {
-        cursor: pointer;
+    g {
+      circle {
+        &:hover {
+          cursor: pointer;
+        }
       }
-      &.highlight {
-        opacity: 1;
-      }
-      &.blur {
-        opacity: 0.2;
-      }
-    }
-    text {
-      font-size: 12px;
-      /*** unselectable ****/
-      -webkit-touch-callout: none;
-      -webkit-user-select: none;
-      -khtml-user-select: none;
-      -moz-user-select: none;
-      -ms-user-select: none;
-      user-select: none;
-      /*********************/
-      &.highlight {
+      text {
         font-size: 12px;
-        font-weight: bold;
-        opacity: 1;
+        /*** unselectable ****/
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+      &.highlight {
+        circle {
+          opacity: 1;
+        }
+        text {
+          font-size: 12px;
+          font-weight: bold;
+          opacity: 1;
+        }
       }
       &.blur {
-        font-size: 10px;
-        opacity: 0.3;
+        circle {
+          opacity: 0.2;
+        }
+        text {
+          font-size: 9px;
+          opacity: 0.2;
+        }
       }
     }
   }

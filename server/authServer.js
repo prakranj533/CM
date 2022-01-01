83e0462e1294fc98c1a2f5755ac39ba923b00b80
Suvrat Ram Joshi <suvrat_joshi@live.com>
2022-01-01 13:11:26 +0545

    integrated mongoose to the register route

--- server/authServer.js
+++ server/authServer.js
@@ -1,21 +1,29 @@
 require('dotenv').config();
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const db = process.env.MONGO_DATABASE_NAME;
+
+mongoose.connect(
+  `mongodb://localhost/${db}`,
+  () => console.log(`Connected to database: ${db}`),
+  e => console.error(e)
+);
+
 const app = express();
-
 app.use(express.json());
-
-const users = [];
 
 app.post('/register', async (req, res) => {
   const firstName = req.body.first_name;
   const lastName = req.body.last_name;
   const email = req.body.email;
-  const dob = `${req.body.year}-${req.body.month}-${req.body.date}`;
+  const dob = req.body.dob;
 
-  if(doesUserExists(email)) {
+  if(await doesUserExists(email)) {
     return res.json({
       success: false,
       message: "Email address already in use"
@@ -24,7 +32,7 @@ app.post('/register', async (req, res) => {
 
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    users.push({
+    await User.create({
       first_name: req.body.first_name,
       last_name: req.body.last_name,
       email: req.body.email,
@@ -44,8 +52,10 @@ app.post('/register', async (req, res) => {
   }
 });
 
-function doesUserExists(email) {
-  return users.find(user => user.email === email);
+async function doesUserExists(email) {
+  const user = await User.findOne({ email });
+  if(user) return true;
+  return false;
 }
 
 function generateAccessToken(user) {

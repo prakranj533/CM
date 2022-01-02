39ffc1213e0096485a288559d6c052c87b9aa3f0
Suvrat Ram Joshi <suvrat_joshi@live.com>
2022-01-02 14:44:45 +0545

    added authorization header as default settings for axios

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_URL = process.env.VUE_APP_SERVER;
+axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem('token');
+
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: API_URL
 });
 
 export default api;

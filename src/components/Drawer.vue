82d569d8cc030a2172950b6a8a6ef2f2eebf44b8
Yograj Gupta <yograj.gupta@criticalcontrol.com>
2023-02-13 13:49:18 +0530

    Drawer menu updated

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -23,6 +23,8 @@ export default {
     items: [
       { title: "Career Map", icon: "mdi-view-dashboard", href:"/admin/dashboard" },
       { title: "Manage Student", icon: "mdi-view-dashboard", href:"/admin/student/list" },
+      { title: "Manage School", icon: "mdi-view-dashboard", href:"/admin/school/list" },
+      { title: "Manage Device", icon: "mdi-image", href:"/admin/device/list" },
       { title: "Set Video", icon: "mdi-image", href:"/admin/set-video" },
       { title: "Manage Counsellor", icon: "mdi-image", href:"/admin/counsellor/list" },
       { title: "Reply to Student/User", icon: "mdi-image", href:"/admin/query/list" },

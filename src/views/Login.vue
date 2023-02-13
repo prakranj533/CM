9e5feeaa3cc020065e1702966070813824ba18e4
Yograj Gupta <yograj.gupta@criticalcontrol.com>
2023-02-13 13:44:41 +0530

    Login email regex fixes

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -62,7 +62,7 @@ export default {
     showPassword: false,
     emailRules: [
       v => !!v || 'Email is required',
-      v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Email must be valid',
+      v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,7})+$/.test(v) || 'Email must be valid',
     ],
     passwordRules: [
       v => !!v || 'Password is required'
